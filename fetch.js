4a2f1729d3e06aa8eaba835970fe9f5b993c6ad1
Kedar.K <kedarkodgire.kk@gmail.com>
2021-02-28 23:41:27 +0530

    delete

--- fetch.js
+++
@@ -1,6 +0,0 @@
-fetch("https://dev.to/api/articles?username=kedark")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
-
-console.log(response);
